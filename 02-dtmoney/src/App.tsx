ded37523e21a20490ac18624c0bb95febbf26232
jeanalcides <jean.ap44@gmail.com>
2022-04-27 22:26:14 -0300

    Trabalhando com context na aplicação

--- 02-dtmoney/src/App.tsx
+++ 02-dtmoney/src/App.tsx
@@ -4,11 +4,13 @@ import { GlobalStyle } from "./styles/global";
 import Modal from 'react-modal'
 import { useState } from "react";
 import { NewTransactionModal } from "./components/NewTransactionModal";
+import { TransactionsProvider } from "./TransactionsContext";
 
 Modal.setAppElement('#root');
 
 export function App() {
   const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
 
   function handleOpenNewTransactionModal(){
       setIsNewTransactionModalOpen(true);
@@ -19,13 +21,17 @@ export function App() {
 
   }
   return (
-    <>
+    <TransactionsProvider>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
+
       <Dashboard />
+
       <NewTransactionModal
       isOpen={isNewTransactionModalOpen}
       onRequestClose={handleCloseNewTransactionModal} />
+
       <GlobalStyle />
-    </>
+
+    </TransactionsProvider>
   );
 }
--- 02-dtmoney/src/components/NewTransactionModal/index.tsx
+++ 02-dtmoney/src/components/NewTransactionModal/index.tsx
@@ -3,8 +3,8 @@ import { Container, RadioBox, TransactionTypeContainer } from './styles';
 import closeImg from '../../assets/close.svg';
 import incomeImg from '../../assets/income.svg'
 import outcomeImg from '../../assets/outcome.svg'
-import { FormEvent, useState } from 'react';
-import { api } from '../../services/api';
+import { FormEvent, useState, useContext } from 'react';
+import { TransactionsContext } from '../../TransactionsContext';
 
 interface NewTransactionProps {
     isOpen: boolean;
@@ -12,22 +12,30 @@ interface NewTransactionProps {
 }
 
 export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionProps) {
+
+    const { createTransaction } = useContext(TransactionsContext);
+
     const [title, setTitle] = useState('');
-    const [value, setValue] = useState(0);
+    const [amount, setAmount] = useState(0);
     const [category, setCategory] = useState('');
     const [type, setType] = useState('deposit');
 
-    function handleCreateNewTransaction(event: FormEvent){
+    async function handleCreateNewTransaction(event: FormEvent){
         event.preventDefault();
-        const data = {
-            title,
-            value,
-            type,
-            category
-        }
-        api.post('/transactions', data)
-    }
 
+        await createTransaction({
+            title,
+            amount,
+            type,
+            category,
+        })
+
+        setTitle('')
+        setAmount(0)
+        setCategory('')
+        setType('')
+        onRequestClose();
+    }
 
     return (
         <Modal
@@ -55,8 +63,8 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionPr
                 <input 
                     type="number"
                     placeholder='Valor'
-                    value={value}
-                    onChange={e => setValue(Number(e.target.value))}
+                    value={amount}
+                    onChange={e => setAmount(Number(e.target.value))}
                  />
 
                 <TransactionTypeContainer> 
